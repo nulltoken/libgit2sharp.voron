cc1c4892314c203ee98e22d2dd9b81d04f60f2d0
nulltoken <emeric.fermas@gmail.com>
2013-08-28 11:34:00 +0200

    Add dependencies build tooling

---
+++ build-dependencies.cmd
@@ -0,0 +1,33 @@
+SETLOCAL
+
+SET BaseDir=%~dp0
+SET FrameworkVersion=v4.0.30319
+SET FrameworkDir=%SystemRoot%\Microsoft.NET\Framework
+SET LibsDir=%BaseDir%Libs\
+
+SET Flavor=Release
+IF [%1] NEQ [] (SET Flavor=%~1)
+
+RMDIR /S /Q "%LibsDir%NativeBinaries"
+DEL "%LibsDir%LibGit2Sharp.*"
+DEL "%LibsDir%CopyNativeDependencies.targets"
+DEL "%LibsDir%Voron.*"
+
+PUSHD "%BaseDir%libgit2sharp"
+"%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "CI-build.msbuild" /target:Build /property:Configuration=%Flavor%
+
+XCOPY ".\LibGit2Sharp\bin\%Flavor%\*.*" "%LibsDir%" /S
+XCOPY ".\LibGit2Sharp\CopyNativeDependencies.targets" "%LibsDir%" /S
+
+POPD
+
+PUSHD "%BaseDir%raven.voron"
+"%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "Voron\Voron.csproj" /property:Configuration=%Flavor%
+
+XCOPY ".\Voron\bin\%Flavor%\*.*" "%LibsDir%" /S
+
+POPD
+
+ENDLOCAL
+
+EXIT /B %ERRORLEVEL%
