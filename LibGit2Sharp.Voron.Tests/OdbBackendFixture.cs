b2e8842d04744abca8faa4f36419b0a5c0311955
nulltoken <emeric.fermas@gmail.com>
2013-08-28 16:59:03 +0200

    Implement Read, Write and Exists

---
+++ LibGit2Sharp.Voron.Tests/OdbBackendFixture.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace LibGit2Sharp.Voron.Tests
+{
+    public class OdbBackendFixture : IDisposable
+    {
+        private static readonly string TempPath = Path.Combine(Path.GetTempPath(), "Voron.OdbBackend");
+
+        public OdbBackendFixture()
+        {
+            Directory.CreateDirectory(TempPath);
+        }
+
+        public void Dispose()
+        {
+            Helper.DeleteDirectory(TempPath);
+        }
+
+        private static Repository InitNewRepository(string baseDir)
+        {
+            string tempPath = Path.Combine(baseDir, Guid.NewGuid().ToString());
+            string path = Repository.Init(tempPath);
+
+            var repository = new Repository(path);
+
+            return repository;
+        }
+
+        private Repository Build(bool isVoronBased)
+        {
+            var repository = InitNewRepository(TempPath);
+
+            if (!isVoronBased)
+            {
+                return repository;
+            }
+
+            var dir = new DirectoryInfo(repository.Info.WorkingDirectory);
+
+            string voronDataFilename = string.Format("{0}-voron.data", dir.Name.Substring(0, 7));
+            string voronDataPath = Path.Combine(TempPath, voronDataFilename);
+
+            repository.ObjectDatabase.AddBackend(new VoronOdbBackend(voronDataPath), priority: 5);
+
+            return repository;
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CanStageAFileAndLookupTheGeneratedBlob(bool isVoronBased)
+        {
+            using (var repo = Build(isVoronBased))
+            {
+                const string path = "test.txt";
+                const string content = "test\n";
+
+                var objectId = new ObjectId("9daeafb9864cf43055ae93beb0afd6c7d144bfa4");
+
+                //TODO: This triggers two calls to Exists() Why?
+                Assert.False(repo.ObjectDatabase.Contains(objectId));
+                Assert.Null(repo.Lookup<Blob>(objectId));
+
+                Helper.Touch(repo.Info.WorkingDirectory, path, content);
+                repo.Index.Stage(path);
+
+                var ie = repo.Index[path];
+                Assert.NotNull(ie);
+                Assert.Equal(objectId, ie.Id);
+
+                Assert.True(repo.ObjectDatabase.Contains(ie.Id));
+
+                //TODO: Maybe lookup of a blob should only trigger read_header()
+                var blob = repo.Lookup<Blob>(objectId);
+                Assert.Equal(content.Length, blob.Size);
+                Assert.Equal(content, blob.ContentAsText());
+            }
+        }
+
+        private static void AddCommitToRepo(Repository repo)
+        {
+            const string path = "test.txt";
+            const string content = "test\n";
+
+            Helper.Touch(repo.Info.WorkingDirectory, path, content);
+            repo.Index.Stage(path);
+
+            var author = new Signature("nulltoken", "emeric.fermas@gmail.com", DateTimeOffset.Parse("Wed, Dec 14 2011 08:29:03 +0100"));
+            repo.Commit("Initial commit", author, author);
+        }
+
+        private static void AssertGeneratedShas(Repository repo)
+        {
+            Commit commit = repo.Commits.Single();
+            Assert.Equal("1fe3126578fc4eca68c193e4a3a0a14a0704624d", commit.Sha);
+            Tree tree = commit.Tree;
+            Assert.Equal("2b297e643c551e76cfa1f93810c50811382f9117", tree.Sha);
+
+            GitObject blob = tree.Single().Target;
+            Assert.IsAssignableFrom<Blob>(blob);
+            Assert.Equal("9daeafb9864cf43055ae93beb0afd6c7d144bfa4", blob.Sha);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CanGeneratePredictableObjectShas(bool isVoronBased)
+        {
+            using (var repo = Build(isVoronBased))
+            {
+                AddCommitToRepo(repo);
+
+                AssertGeneratedShas(repo);
+            }
+        }
+    }
+}
---
+++ LibGit2Sharp.Voron/VoronOdbBackend.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Voron;
+using Voron.Impl;
+
+namespace LibGit2Sharp.Voron
+{
+    public class VoronOdbBackend : OdbBackend
+    {
+        private const string MetaPrefix = "meta/";
+        private const string DataPrefix = "data/";
+
+        private readonly StorageEnvironment _env;
+
+        public VoronOdbBackend(string voronDataPath)
+        {
+            if (voronDataPath == null)
+            {
+                throw new ArgumentNullException("voronDataPath");
+            }
+
+            var memoryMapPager = new MemoryMapPager(voronDataPath);
+            _env = new StorageEnvironment(memoryMapPager);
+        }
+
+        protected override void Dispose()
+        {
+            _env.Dispose();
+            base.Dispose();
+        }
+        public override int Read(ObjectId id, out Stream data, out ObjectType objectType)
+        {
+            using (var tx = _env.NewTransaction(TransactionFlags.Read))
+            {
+                using (var stream = _env.Root.Read(tx, ToMetaId(id)))
+                {
+                    Debug.Assert(stream != null);
+
+                    var bf = new BinaryFormatter();
+                    var meta = (ObjectDescriptor)bf.Deserialize(stream);
+
+                    objectType = meta.ObjectType;
+                    data = Allocate(meta.Length);
+                }
+
+                using (var stream = _env.Root.Read(tx, ToDataId(id)))
+                {
+                    stream.CopyTo(data);
+                }
+            }
+
+            return 0;
+        }
+
+        public override int ReadPrefix(byte[] shortOid, int prefixLen, out byte[] oid, out Stream data, out ObjectType objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ReadHeader(ObjectId id, out int length, out ObjectType objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Write(ObjectId id, Stream dataStream, long length, ObjectType objectType)
+        {
+            var objDescriptor = new ObjectDescriptor { Length = length, ObjectType = objectType };
+
+            var bf = new BinaryFormatter();
+
+            using (var ms = new MemoryStream())
+            using (var tx = _env.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                bf.Serialize(ms, objDescriptor);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                _env.Root.Add(tx, ToMetaId(id), ms);
+                _env.Root.Add(tx, ToDataId(id), dataStream);
+                tx.Commit();
+            }
+
+            return 0;
+        }
+
+        private static string ToDataId(ObjectId id)
+        {
+            return DataPrefix + id.Sha;
+        }
+
+        private static string ToMetaId(ObjectId id)
+        {
+            return MetaPrefix + id.Sha;
+        }
+
+        public override int ReadStream(ObjectId id, out OdbBackendStream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int WriteStream(long length, ObjectType objectType, out OdbBackendStream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Exists(ObjectId id)
+        {
+            using (var tx = _env.NewTransaction(TransactionFlags.Read))
+            using (var stream = _env.Root.Read(tx, ToMetaId(id)))
+            {
+                return stream != null;
+            }
+        }
+
+        public override int ForEach(ForEachCallback callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override OdbBackendOperations SupportedOperations
+        {
+            get
+            {
+                return OdbBackendOperations.Read |
+                       OdbBackendOperations.Write |
+                       OdbBackendOperations.Exists;
+            }
+        }
+
+        [Serializable]
+        private class ObjectDescriptor
+        {
+            public ObjectType ObjectType { get; set; }
+            public long Length { get; set; }
+        }
+    }
+}
